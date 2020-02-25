3069f3212b8ad099b7ae2dcda8f557e5e30d015f
pathas <pathas@naver.com>
2020-02-26 00:08:58 +0900

    State, LifeCycle

--- src/App.js
+++ src/App.js
@@ -1,46 +1,64 @@
-import React from "react";
+import React, { Component } from "react";
 // props의 타입을 체크해주는 라이브러리
 // 이름은 반드시 propTypes로 작성, 그래야 리액트가 확인을 할 수 있음
 import PropTypes from "prop-types";
 
-function Food({ name, desc, rating }) {
-  return (
-    <div>
-      <h1>I like {name}</h1>
-      <h4>{rating}/10</h4>
-      <h2>this is {desc}</h2>
-    </div>
-  );
-}
+class App extends Component {
+  // 자바스크립트 class 문법에 따른 생성자
+  // 리액트에 종속된 문법이 아님
+  constructor(props) {
+    super(props);
+    console.log("constructor");
+  }
 
-// props 타입 검사
-// PropTypes.자료형 으로 작성, isRequired는 필수 속성에 부여
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  desc: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-};
+  // 변경될 여지가 있는 데이터를 state로 관리함
+  state = {
+    count: 0
+  };
+  add = count => {
+    console.log("add");
+    // setState 내부에 current를 매개변수로 사용하는 함수 작성 가능
+    // 외부 state에 의존하지 않는 가장 좋은 방법
+    // 현재 state에서 새로운 state를 얻어내서 작업(불변성과 관련된 듯)
+    this.setState(current => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    console.log("minus");
+    // setState함수가 호출될 때마다 새로운 state와 함께
+    // render() 함수가 호출됨
+    this.setState(current => ({ count: current.count - 1 }));
+  };
 
-const foodILike = [
-  { id: 1, name: "kimchi", desc: "hot", rating: 5 },
-  { id: 2, name: "ramen", desc: "hooooot", rating: 6 },
-  { id: 3, name: "samgiopsal", desc: "hmm", rating: 10 },
-  { id: 4, name: "chukumi", desc: "spicy", rating: 9 }
-];
+  // 컴포넌트가 처음 생성되었을 때만 호출되는 함수
+  // API연결에 주로 사용
+  componentDidMount() {
+    console.log("component rendered");
+  }
 
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (
-        <Food
-          key={dish.id}
-          name={dish.name}
-          desc={dish.desc}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
+  componentDidUpdate() {
+    console.log("I did Update");
+  }
+
+  componentWillUnmount() {
+    console.log("Good bye, cruel world");
+  }
+
+  render() {
+    console.log("I'm rendering");
+
+    // state가 컴포넌트 내부에 있기 때문에 this.state로 작성
+    const { count } = this.state;
+
+    return (
+      <div>
+        <h1>The number is {count}</h1>
+        {/* 이벤트리스너에는 ()를 작성하지 않음
+        ()는 함수를 즉시 실행하는 것을 뜻함*/}
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
